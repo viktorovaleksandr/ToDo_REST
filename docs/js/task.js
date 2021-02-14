6c77a9aa413114688a8882a849d1826a0b491b7e
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-02-14 13:59:45 +0200

    add class

--- docs/js/task.js
+++ docs/js/task.js
@@ -1,15 +1,4 @@
 class TodoRequests {
-	static sendPutTodoRequest(id,todo) {
-		return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-  		method: 'PUT',
-  		body: JSON.stringify(todo),
-  		headers: {
-    		'Content-type': 'application/json; charset=UTF-8',
-  		},
-	})
-  .then((response) => response.json())
-	}
-
 	static sendGetTodosRequest() {
 		return fetch('https://jsonplaceholder.typicode.com/todos').then((response) => response.json())
 	}
@@ -23,6 +12,17 @@ class TodoRequests {
   			},
 		})
   	.then((response) => response.json())
+	}
+
+	static sendPutTodoRequest(id,todo) {
+		return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+  		method: 'PUT',
+  		body: JSON.stringify(todo),
+  		headers: {
+    		'Content-type': 'application/json; charset=UTF-8',
+  		},
+	})
+  .then((response) => response.json())
 	}
 
 	static sendDeleteTodoRequest(id) {
@@ -57,12 +57,12 @@ class TodoLogic {
 	  	});
 	}
 
-	static createTodo() {
+	static createTodos() {
 		const todo = getTodoFormData();
 
 		const promise = TodoRequests.sendPostTodoRequest(todo);
 		promise.then(todo => { 
-			clearForm();
+			clearInput();
 			renderTodo(todo);
 			todosRopository.todos.push(todo); 
 		});
@@ -74,24 +74,23 @@ class TodoLogic {
 
 		const promise = TodoRequests.sendDeleteTodoRequest(id);
 		promise.then(() => {
-			const listId = ulTodoElement.querySelector(`li[data-id="${id}"]`);
-			listId.remove();
+			const listElementId = ulTodoElement.querySelector(`li[data-id="${id}"]`);
+			listElementId.remove();
 			todosRopository.todos = todosRopository.todos.filter(todo => todo.id !== id);
 		});
 	}
 
-	static upDateTodo(event) {
+	static updateTodo(event) {
 		const listElement = event.target.closest('li');
 		const id = parseInt(listElement.dataset.id, 10);
 		const todo = todosRopository.getTodoById(id);
 		
-		todoAssignBooleanData(todo);
+		todoAssignUpdateData(todo);
 		const promise = TodoRequests.sendPutTodoRequest(id,todo);
 
 		promise.then(todo => { 
-			const listId = ulTodoElement.querySelector(`li[data-id="${id}"]`);
-	   	listId.classList.toggle('list-group-item-info');
-	   	listId.classList.toggle('list-group-item-secondary');
+			const listElementId = ulTodoElement.querySelector(`li[data-id="${id}"]`);
+	   	listElementId.classList.toggle('list-group-item-info');
 		});
 	}
 }
@@ -99,6 +98,7 @@ class TodoLogic {
 // INIT TODOS
 
 const todosRopository = new TodosRopository();
+
 const addButtonElement = document.querySelector('#js-add-todo');
 const todoInputElement = document.forms.todo;
 const ulTodoElement = document.querySelector('.js-list-todo');
@@ -115,50 +115,46 @@ function	init() {
 
 function createAddTodoEventListener() {
 	addButtonElement.addEventListener('click', () => {
-      TodoLogic.createTodo();
-   });
+      TodoLogic.createTodos();
+   })
 }
 
 function createTodoActionEventListener() {
 	ulTodoElement.addEventListener('click', (event) => {
-      if(event.target.classList.contains('bi')) {
+
+		if(event.target.closest('li')) {
+		   TodoLogic.updateTodo(event);
+		}
+      if(event.target.closest('i')) {
          TodoLogic.deleteTodo(event);
       }
-      if(event.target.closest('li')) {
-         TodoLogic.upDateTodo(event);
-      }
-   });
+   })
 }
 
 // RENDER 
 
 function renderTodos(todos) {
-	const lists = todos.map((todo) => getListElement(todo));
+	const lists = todos.map((todo) => createListElement(todo));
 }
 
 function renderTodo(todo) {
-	const list = getListElement(todo);	
+	const list = createListElement(todo);	
 }
 
-function getListElement(todo) {
+function createListElement(todo) {
 	const list = document.createElement('li');
 	list.className = `list-group-item list-group-item-action d-flex 
-	justify-content-between rounded-pill`;
+	justify-content-between rounded-pill list-group-item-secondary`;
 	list.dataset.id = todo.id;
 	list.textContent = todo.title;
-	ulTodoElement.prepend(list);
-
 	const closeButton = `<i class="bi bi-trash-fill"></i>`;
-	list.insertAdjacentHTML('beforeend', closeButton);
-	renderTodosCompleted(todo,list);
-}
-
-function renderTodosCompleted(todo,list) {
+	
 	if (todo.completed) {
 		list.classList.add('list-group-item-info');
-	}  else {
-		list.classList.add('list-group-item-secondary');
-	}
+	}  
+
+	ulTodoElement.prepend(list);
+	list.insertAdjacentHTML('beforeend', closeButton);
 }
 
 // FORM UTILS
@@ -171,14 +167,15 @@ function getTodoFormData() {
    }
 }
 
-function clearForm() {
-	todoInputElement.reset();
-}
-
-function todoAssignBooleanData(todo) {
+function todoAssignUpdateData(todo) {
 	if (todo.completed) {
 		todo.completed = false;
 	} else {
 		todo.completed = true;
 	}
 }
+
+function clearInput() {
+	todoInputElement.reset();
+}
+
